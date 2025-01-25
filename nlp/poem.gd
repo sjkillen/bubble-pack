6876f725e07e4fa62bf3910b5127aec3c46ed6c0
Rita Lester <r.a.lester121@gmail.com>
2025-01-25 15:16:14 -0700

    add poem.next()

--- nlp/poem.gd
+++ nlp/poem.gd
@@ -1,11 +1,24 @@
 extends Node2D
 
+var ix = 0
+
+var lines = []
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass
+	lines = get_children()
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
 	pass
+
+func next():
+	if ix >= lines.size():
+		ix = 0
+	
+	var ret = lines[ix]
+	
+	ix += 1
+	
+	return ret
